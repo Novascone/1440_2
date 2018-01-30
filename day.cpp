1b0cf2320a287aa406691f5e04104bcd02e98022
Warren  Novascone <novasconewarren@gmail.com>
2018-01-30 02:01:22 +0000

    second class

---
+++ day.cpp
@@ -0,0 +1,23 @@
+#include "stat.hpp"
+#include "day.hpp"
+#include <iostream>
+
+Stat* Day::getStatNext() {
+	Stat* stat = nullptr;
+	if (_currentStat < _statCount)
+		stat = _stats[_currentStat++];
+	return stat;
+}
+
+Day::Day(std::string date) {
+	_date = date;
+}
+void Day::resetStatIteration() {
+	_currentStat = 0;
+}
+void Day::addStat(Stat *stat) {
+	if (_statCount<MAX_STATS)
+		_stats[_statCount++] = stat;
+	else
+		std::cerr << "Too many stats for " << getDate() << std::endl;
+}
---
+++ day.hpp
@@ -0,0 +1,21 @@
+#ifndef day_hpp
+#define day_hpp
+#include <string>
+#define MAX_STATS         100
+
+
+class Day {
+	private:
+		std::string         _date;
+		Stat*               _stats[MAX_STATS];
+		int                 _statCount = 0;
+		int                 _currentStat = -1;
+
+	public:
+		Day(std::string date);
+		void addStat(Stat *stat);
+		void resetStatIteration();
+		Stat* getStatNext();
+		std::string getDate() { return _date; }
+};
+#endif

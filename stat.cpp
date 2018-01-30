45ab108d22d90ba442210c6631a813050ae1afdf
Warren  Novascone <novasconewarren@gmail.com>
2018-01-30 01:39:44 +0000

    it works finally

--- stat.cpp
+++ stat.cpp
@@ -1,15 +1,16 @@
 
-#include "stat.hpp"
-#include <iostream>
-using namespace std;
+// #include "stat.hpp"
+// #include <iostream>
+// using namespace std;
 
-Stat::Stat(std::string time, std::string qgag, std::string qpcp) {
-	_time = time;
-	_precipitationQgag = stof(qgag);
-	if (_precipitationQgag<= -9999 || _precipitationQgag >= 999)
-		_precipitationQgag = 0;
+// Stat::Stat(std::string time, std::string qgag, std::string qpcp) {
+// 	_time = time;
+// 	_precipitationQgag = stof(qgag);
+// 	if (_precipitationQgag<= -9999 || _precipitationQgag >= 999)
+// 		_precipitationQgag = 0;
 
-	_precipitationQpcp = stof(qpcp);
-	if (_precipitationQpcp<= -9999 || _precipitationQpcp >= 999)
-		_precipitationQpcp = 0;
-}
+// 	_precipitationQpcp = stof(qpcp);
+// 	if (_precipitationQpcp<= -9999 || _precipitationQpcp >= 999)
+// 		_precipitationQpcp = 0;
+// }
+// #endif
--- stat.hpp
+++ stat.hpp
@@ -1,18 +1,18 @@
-#ifndef stat_hpp
-#define stat_hpp
-using namespace std;
+// #ifndef stat_hpp
+// #define stat_hpp
+// using namespace std;
 
-class Stat {
-	private:
-		std::string _time;
-		float _precipitationQgag;  // Precipitation volume in 1/100th of (cubic?) inch
-		float _precipitationQpcp;  // Precipitation amount in 1/100th of inch
+// class Stat {
+// 	private:
+// 		std::string _time;
+// 		float _precipitationQgag;  // Precipitation volume in 1/100th of (cubic?) inch
+// 		float _precipitationQpcp;  // Precipitation amount in 1/100th of inch
 
-	public:
-		Stat(std::string time, std::string qgag, std::string cpcp);
-		std::string getTime() { return _time; }
-		float getPrecipitationQgag() { return _precipitationQgag; }
-		float getPrecipitationQpcp() { return _precipitationQpcp; }
-};
+// 	public:
+// 		Stat(std::string time, std::string qgag, std::string cpcp);
+// 		std::string getTime() { return _time; }
+// 		float getPrecipitationQgag() { return _precipitationQgag; }
+// 		float getPrecipitationQpcp() { return _precipitationQpcp; }
+// };
 
-#endif
+// #endif
